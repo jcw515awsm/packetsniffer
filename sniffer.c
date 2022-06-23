7e90426cf25537cf7efca2d8bfaaab32bcf172e1
jcw515awsm <awsmguytoo@gmail.com>
2022-06-22 20:36:56 -0500

    tutorial & basic socket development

--- sniffer.c
+++ sniffer.c
@@ -1,6 +1,7 @@
 
 
-int main()
+sock_raw = socket(AF_INET , SOCK_RAW , IPPROTO_TCP);
+while(1)
 {
-	puts ("hi I am a packe sniffer");
-}
+data_size = recvfrom(sock_raw, buffer , 65536 , 0 , &saddr , &saddr_size);
+
---
+++ socketprogrammingtutorial.c
@@ -0,0 +1,23 @@
+
+commands-
+socket() - endpoint for communication
+bind() - assign a unique telephone number
+listen() - wait for a caller
+connect() - dial a number
+accept() - receive a call
+send(), recv() - talk
+close() - hang up 
+
+client server model
+
+server -  provider of information
+client - seeker of information
+
+
+
+
+
+
+
+
+
